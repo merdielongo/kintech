96d44a02b5350dd2a58012d0ca67306ba6555e0b
merdielongo <merdielongo@github.com>
2021-09-29 15:31:09 +0100

    upgrade home

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -40,12 +40,12 @@
                 </div>
 
                 <div class="mt-5 col-lg-6 col-md-6">
-                    <span class="text-white fw-normal fs-5">Organisé par le commissariat provinciale en charge du numérique</span>
+                    <span class="text-white fw-normal fs-5">Organisé par le commissariat provincial en charge du numérique</span>
                     <div class="mt-3 row">
                         <div class="col-lg-12">
                             <div class="flex-row p-3 mb-2 border-0 d-flex" style="background: #A3386D">
                                 <div class="text-white fas fa-check-circle fa-2x pe-4"></div>
-                                <div class="text-white fw-bold">2 jours d’évènement</div>
+                                <div class="text-white fw-bold">2 jours d’évènements</div>
                             </div>
 
                             <div class="flex-row p-3 mb-2 border-0 d-flex" style="background: #A3386D">
@@ -66,13 +66,74 @@
   </header>
 
 
-<div class="p-3 mt-5 bg-white">
+{{-- <div class="p-3 mt-5 bg-white">
     <div class="p-4 row">
         <center>
             <div class="fs-4 fw-bold">
                 <span>Innovation - Découverte - Partenariat</span>
             </div>
         </center>
+    </div>
+</div> --}}
+
+<div class="pt-5 pb-5">
+    <div class="container pt-2 pb-2">
+        {{-- <div class="row">
+            <center>
+                <span class="text-2xl font-bold">EDITION 2021</span> <br>
+            </center>
+        </div><br><br><br> --}}
+
+        <div class="row">
+            <div class="mb-5 col-lg-3 col-md-3">
+                <div id="card-intervenant rounded-3">
+                    <div class="mt-2 border-0 card rounded-2xl">
+                        <div class="card-body">
+                            <br><h5 class="font-bold text-center card-title">Vélocité</h5> <br>
+                            <p class="mb-4 text-center card-text">Transformation Numérique à Vitesse Grand V</p>
+                        </div>
+                        <div class="p-1 rounded-b-2xl" style="background: #8D3D89"></div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="mb-5 col-lg-3 col-md-3">
+                <div id="card-intervenant rounded-3">
+                    <div class="mt-2 border-0 card rounded-2xl">
+                        <div class="card-body">
+                            <br><h5 class="font-bold text-center card-title">Innovation</h5> <br>
+                            <p class="text-center card-text">Ensemble mettons en avant la Technologie, la Technicité et la Créativité</p>
+                        </div>
+                        <div class="p-1 rounded-b-2xl" style="background: #8D3D89"></div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="mb-5 col-lg-3 col-md-3">
+                <div id="card-intervenant rounded-3">
+                    <div class="mt-2 border-0 card rounded-2xl">
+                        <div class="card-body">
+                            <br><h5 class="font-bold text-center card-title">Avenir</h5> <br>
+                            <p class="mb-4 text-center card-text">Créons notre propre Patrimoine Intellectuel</p>
+                        </div>
+                        <div class="p-1 rounded-b-2xl" style="background: #8D3D89"></div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="mb-5 col-lg-3 col-md-3">
+                <div id="card-intervenant rounded-3">
+                    <div class="mt-2 border-0 card">
+                        <div class="card-body">
+                            <br><h5 class="font-bold text-center card-title">Découverte</h5> <br>
+                            <p class="mb-4 text-center card-text">Ensemble créons nos propres Solutions</p>
+                        </div>
+                        <div class="p-1 rounded-b-2xl" style="background: #8D3D89"></div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
     </div>
 </div>
 
@@ -103,17 +164,17 @@
             </center>
         </div>
 
+
         <div class="mb-4 col-lg-3 col-md-3">
             <div class="shadow-sm card">
                 <div class="img-card-top">
-                    <img src="{{ asset('img/thumb/t3.jpg') }}" alt="" class="w-100">
+                    <img src="{{ asset('img/thumb/t1.jpg') }}" alt="" class="w-100">
                 </div>
 
                 <div class="p-4 text-center card-content">
-                    <p class="mt-1 fw-bold">Devenez sponsor <br><br> <br></p>
-                    <a href="#" class="btn btn-danger">En savoir plus</a>
+                    <a class="mt-1 text-black fw-bold" href="{{ route('home.exposition') }}">Devenez exposant. <br> <br> <br> <br> </a>
+                    {{-- <a href="#" class="btn btn-danger">En savoir plus</a> --}}
                 </div>
-                <br>
             </div>
         </div>
 
@@ -124,8 +185,8 @@
                 </div>
 
                 <div class="p-4 text-center card-content">
-                    <p class="mt-1 fw-bold">Inscrivez –vous en ligne gratuitement et participez à l’évènement en tant que visiteur </p>
-                    <a href="#" class="btn btn-danger">En savoir plus</a>
+                    <a class="mt-1 text-black fw-bold" href="{{ route('home.register') }}">Inscrivez –vous en ligne gratuitement et participez à l’évènement en tant que visiteur </a>
+                    {{-- <a href="#" class="btn btn-danger">En savoir plus</a> --}}
                 </div>
             </div>
         </div>
@@ -133,15 +194,17 @@
         <div class="mb-4 col-lg-3 col-md-3">
             <div class="shadow-sm card">
                 <div class="img-card-top">
-                    <img src="{{ asset('img/thumb/t1.jpg') }}" alt="" class="w-100">
+                    <img src="{{ asset('img/thumb/t3.jpg') }}" alt="" class="w-100">
                 </div>
 
                 <div class="p-4 text-center card-content">
-                    <p class="mt-1 fw-bold">Devenez exposant. <br> <br> <br> </p>
-                    <a href="#" class="btn btn-danger">En savoir plus</a>
+                    <a class="mt-1 text-black fw-bold" href="{{ route('home.sponsor') }}">Devenez sponsor <br><br> <br> <br></a>
+                    {{-- <a href="#" class="btn btn-danger">En savoir plus</a> --}}
                 </div>
+                <br>
             </div>
         </div>
+
 
 
         <div class="mb-4 col-lg-3 col-md-3">
@@ -157,65 +220,7 @@
 </div>
 
 
-<div class="pt-5 pb-5">
-    <div class="container pt-2 pb-2">
-        <div class="row">
-            <center>
-                <span class="text-2xl font-bold">EDITION 2021</span> <br>
-            </center>
-        </div><br><br><br>
 
-        <div class="row">
-            <div class="mb-5 col-lg-3 col-md-3">
-                <div id="card-intervenant rounded-3">
-                    <div class="mt-2 border-0 card rounded-2xl">
-                        <div class="card-body">
-                            <br><h5 class="font-bold text-center card-title">Vélocité</h5> <br>
-                            <p class="mb-4 text-center card-text">Transformation Numérique à Vitesse Grand V</p>
-                        </div>
-                        <div class="p-1 rounded-b-2xl" style="background: #8D3D89"></div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="mb-5 col-lg-3 col-md-3">
-                <div id="card-intervenant rounded-3">
-                    <div class="mt-2 border-0 card">
-                        <div class="card-body">
-                            <br><h5 class="font-bold text-center card-title">Découverte</h5> <br>
-                            <p class="mb-4 text-center card-text">Ensemble créons nos propres Solutions</p>
-                        </div>
-                        <div class="p-1 rounded-b-2xl" style="background: #8D3D89"></div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="mb-5 col-lg-3 col-md-3">
-                <div id="card-intervenant rounded-3">
-                    <div class="mt-2 border-0 card rounded-2xl">
-                        <div class="card-body">
-                            <br><h5 class="font-bold text-center card-title">Innovation</h5> <br>
-                            <p class="text-center card-text">Ensemble mettons en avant la Technologie, la Technicité et la Créativité</p>
-                        </div>
-                        <div class="p-1 rounded-b-2xl" style="background: #8D3D89"></div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="mb-5 col-lg-3 col-md-3">
-                <div id="card-intervenant rounded-3">
-                    <div class="mt-2 border-0 card rounded-2xl">
-                        <div class="card-body">
-                            <br><h5 class="font-bold text-center card-title">Avenir</h5> <br>
-                            <p class="mb-4 text-center card-text">Créons notre propre Patrimoine Intellectuel</p>
-                        </div>
-                        <div class="p-1 rounded-b-2xl" style="background: #8D3D89"></div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
 
 
 <div style="background: #A3386D">
