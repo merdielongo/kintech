bf4bc50cdd15db0f09de1fd8d94dba4f310f4898
Merdi Elongo <merdielongo9@gmail.com>
2022-12-10 18:42:49 +0100

    change custom atelie page

--- resources/views/pages/ateliers.blade.php
+++ resources/views/pages/ateliers.blade.php
@@ -4,22 +4,44 @@
 
     <div class="container mt-5 mb-3">
 
-        <div class="row p-4">
+        <div class="p-4 row">
 
-            <div class="col-12 p-5">
+            <div class="p-5 col-12">
                 <center>
                     <h4 class="text-upper">PARTICIPEZ AUX ATELIERS ARTS NUMÉRIQUES</h4>
                 </center>
             </div>
 
-            <div class="col-lg-4 col-md-4 mb-3">
-                <img src="{{ asset('img/at_1.JPG') }}">
+            <div class="mb-3 col-lg-3 col-md-3">
+                <img src="{{ asset('img/at/1.jpg') }}">
             </div>
-            <div class="col-lg-4 col-md-4 mb-3">
-                <img src="{{ asset('img/at_2.JPG') }}">
+
+            <div class="mb-3 col-lg-3 col-md-3">
+                <img src="{{ asset('img/at/2.jpg') }}">
             </div>
-            <div class="col-lg-4 col-md-4 mb-3">
-                <img src="{{ asset('img/at_3.JPG') }}">
+
+            <div class="mb-3 col-lg-3 col-md-3">
+                <img src="{{ asset('img/at/3.jpg') }}">
+            </div>
+
+            <div class="mb-3 col-lg-3 col-md-3">
+                <img src="{{ asset('img/at/4.jpg') }}">
+            </div>
+
+            <div class="mb-3 col-lg-3 col-md-3">
+                <img src="{{ asset('img/at/5.jpg') }}">
+            </div>
+
+            <div class="mb-3 col-lg-3 col-md-3">
+                <img src="{{ asset('img/at/6.jpg') }}">
+            </div>
+
+            <div class="mb-3 col-lg-3 col-md-3">
+                <img src="{{ asset('img/at/7.jpg') }}">
+            </div>
+
+            <div class="mb-3 col-lg-3 col-md-3">
+                <img src="{{ asset('img/at/8.jpg') }}">
             </div>
         </div>
 
